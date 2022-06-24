40b097d60e3c0defbf4b9d24548a27209777c642
rkessal <kessal.rayhan25@gmail.com>
2022-06-24 14:33:35 +0200

    First commit

---
+++ vues/v_bandeau.php
@@ -0,0 +1,12 @@
+<div id="bandeau">
+<!-- Images En-tête -->
+<!--<img src="images/menuGauche.gif"	alt="Choisir" title="Choisir"/>-->
+<img src="images/logo.png" style="width:200px; height:126px;"	alt="Lafleur" title="Lafleur"/>
+</div>
+<!--  Menu haut-->
+<ul id="menu">
+	<li><a href="index.php?uc=accueil"> Accueil </a></li>
+	<li><a href="index.php?uc=voirProduits&action=voirCategories"> Voir le catalogue de fleurs </a></li>
+	<li><a href="index.php?uc=gererPanier&action=voirPanier"> Voir son panier </a></li>
+	<li><a href="index.php?uc=administrer"> Administrer </a></li>
+</ul>
